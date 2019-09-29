3f3c6a1afc659a19f9e47403d35d89ae565ed360
Akhmad Yulian F <akhmadyulianfebriannur22@gmail.com>
2019-09-30 05:48:43 +0800

    kategori_berita

---
+++ app/Http/Controllers/KategoriBeritaController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\KategoriBerita;
+
+class KategoriBeritaController extends Controller
+{
+    public function index(){
+        $listKategoriBerita=KategoriBerita::all();
+
+        return view('kategori_berita.index', compact('listKategoriBerita'));
+    }
+
+    public function show($id){
+        $kategoriBerita=KategoriBerita::find($id);
+
+        return view('kategori_berita.show', compact('kategoriBerita'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('refresh_captcha', 'HomeController@refreshCaptcha')->name('refresh_captcha');
 Route::get('/kategori_artikel', 'KategoriArtikelController@index')->name('kategori_artikel.index');
 Route::get('/kategori_artikel/{id}','KategoriArtikelController@show')->name('kategori_artikel.show');
-
+Route::get('/kategori_berita', 'KategoriBeritaController@index')->name('kategori_berita.index');
+Route::get('/kategori_berita/{id}','KategoriBeritaController@show')->name('kategori_berita.show');
